e1313f043abd208e5a708a65ba32da7f00611643
Phanindra1989 <82647234+Phanindra1989@users.noreply.github.com>
2021-05-25 18:54:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World test 123
+         Hello World welcome to my first react app
         </p>
         <a
           className="App-link"
