578a06a197180756d5f540b0e30c6f4863266a28
Phanindra1989 <phanindra.t@nuncsystems.com>
2021-05-26 11:45:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World welcome
+         Hello World welcome phani 122222
         </p>
         <a
           className="App-link"
